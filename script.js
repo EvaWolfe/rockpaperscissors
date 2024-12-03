d70c42eaa4bec047054dda65fb9e6ae06c7d2b37
EvaWolfe <eywolfe@gmail.com>
2024-12-02 22:24:22 -0500

    updates script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ window.alert('Welcome to the Thunderdome!');
 const playerScore = 0;
 const computerScore = 0;
 let playerChoice;
+let computerChoice;
 const choices = ['r', 'p', 's'];
 
 //sets player choice
@@ -19,7 +20,6 @@ function getPlayerChoice() {
     getPlayerChoice();
   }
 };
-getPlayerChoice();
 
 //computer random choice
 function randomComputerChoice() {
@@ -27,39 +27,40 @@ function randomComputerChoice() {
   return choices[randomIndex];
 };
 
-//stores computer choice
-let computerChoice = randomComputerChoice();
-window.alert(`Computer chose: ${computerChoice}!`);
-
 //Figure out who won
 
-//Tie
-if (playerChoice === computerChoice) {
-  window.alert(`It's a tie!`);
-  //player wins
-} else if (
-  (playerChoice === 'r' && computerChoice === 's') ||
-  (playerChoice === 's' && computerChoice === 'p') ||
-  (playerChoice === 'p' && computerChoice === 'r')) {
-  window.alert('Winner, Winner, Chicken Dinner!');
-  //computer wins
-} else {
-  window.alert('Computer Dominates!')
+function determineWinner() {
+  //Tie
+  if (playerChoice === computerChoice) {
+    window.alert(`It's a tie!`);
+    //player wins
+  } else if (
+    (playerChoice === 'r' && computerChoice === 's') ||
+    (playerChoice === 's' && computerChoice === 'p') ||
+    (playerChoice === 'p' && computerChoice === 'r')) {
+    window.alert('Winner, Winner, Chicken Dinner!');
+    //computer wins
+  } else {
+    window.alert('Computer Dominates!')
+  };
 };
+//ask if player wants to play again
 function playAgain() {
   if (window.confirm(`Would you like to play again?`)) {
-    getPlayerChoice();
+    game();
   } else { window.alert(`Thanks for playing!`) };
 };
-playAgain();
+
 
 function game() {
-
+  getPlayerChoice();
+  //stores computer choice
+  computerChoice = randomComputerChoice();
+  window.alert(`Computer chose: ${computerChoice}!`);
+  determineWinner();
+  playAgain();
 };
-
-//ask if player wants to play again
-
-
+game();
 //scoreboard: figure out a way to keep score: wins ties & losses. player is needy so much confirm how many times they chose each weapon...tryhard
 
 //overall game function
