5d5797122e208318565190694f5c2fa3e2667a05
EvaWolfe <eywolfe@gmail.com>
2024-12-02 21:00:36 -0500

    updates script.js adds get playerChoice function

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
+
 //Alert when page loads
 window.alert('Welcome to the Thunderdome!');
 
-const choice = window.prompt('Choose your fighter! R, P, or S!');
+//Alert window to let player make selection
 
-//failsafe for not a valid choice
+
+//global variables
+const playerScore = 0;
+const computerScore = 0;
+let playerChoice;
+const choices = ['r', 'p', 's'];
+
+//sets player choice
+function getPlayerChoice() {
+  playerChoice = window.prompt('Choose your fighter! R, P, or S!');
+
+  //failsafe for invalid choice
+
+  if (!choices.includes(playerChoice)) {
+    window.alert(`Your only options are: R, P, or S! Try again!`);
+    getPlayerChoice();
+  }
+};
+getPlayerChoice();
+
+//figure out who won
+// function winner() {
+//   if (playerChoice = 'r' &&= computerChoice != 's') {
+//     window.alert('Well done! You win!');
+//   }
+// };
+
+//overall game function
+
+// function game() {
+
+// };
 
 
 //computer random choice
 function randomComputerChoice() {
-  const choices = ['r', 'p', 's'];
+
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 };
@@ -17,8 +49,7 @@ function randomComputerChoice() {
 let computerChoice = randomComputerChoice();
 window.alert(`Computer chose: ${computerChoice}!`);
 
-//figure out who won
-
+// function winner()
 //ask if player wants to play again
 
 //scoreboard: figure out a way to keep score: wins ties & losses. player is needy so much confirm how many times they chose each weapon...tryhard
