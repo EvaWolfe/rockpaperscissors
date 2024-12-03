debbd0f12a265dc767254eb0430908d6d29e44a1
EvaWolfe <eywolfe@gmail.com>
2024-12-02 21:59:53 -0500

    adds player outcomes to script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 //Alert when page loads
 window.alert('Welcome to the Thunderdome!');
 
-//Alert window to let player make selection
-
 
 //global variables
 const playerScore = 0;
@@ -13,10 +11,9 @@ const choices = ['r', 'p', 's'];
 
 //sets player choice
 function getPlayerChoice() {
+  //Alert window to let player make selection
   playerChoice = window.prompt('Choose your fighter! R, P, or S!');
-
   //failsafe for invalid choice
-
   if (!choices.includes(playerChoice)) {
     window.alert(`Your only options are: R, P, or S! Try again!`);
     getPlayerChoice();
@@ -24,23 +21,8 @@ function getPlayerChoice() {
 };
 getPlayerChoice();
 
-//figure out who won
-// function winner() {
-//   if (playerChoice = 'r' &&= computerChoice != 's') {
-//     window.alert('Well done! You win!');
-//   }
-// };
-
-//overall game function
-
-// function game() {
-
-// };
-
-
 //computer random choice
 function randomComputerChoice() {
-
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 };
@@ -49,7 +31,35 @@ function randomComputerChoice() {
 let computerChoice = randomComputerChoice();
 window.alert(`Computer chose: ${computerChoice}!`);
 
-// function winner()
+//Figure out who won
+
+//Tie
+if (playerChoice === computerChoice) {
+  window.alert(`It's a tie!`);
+  //player wins
+} else if (
+  (playerChoice === 'r' && computerChoice === 's') ||
+  (playerChoice === 's' && computerChoice === 'p') ||
+  (playerChoice === 'p' && computerChoice === 'r')) {
+  window.alert('Winner, Winner, Chicken Dinner!');
+  //computer wins
+} else {
+  window.alert('Computer Dominates!')
+};
+function playAgain() {
+  if (window.confirm(`Would you like to play again?`)) {
+    getPlayerChoice();
+  } else { window.alert(`Thanks for playing!`) };
+};
+playAgain();
+
+function game() {
+
+};
+
 //ask if player wants to play again
 
+
 //scoreboard: figure out a way to keep score: wins ties & losses. player is needy so much confirm how many times they chose each weapon...tryhard
+
+//overall game function
